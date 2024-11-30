98de4353d1002fe8c066006e74be42ebd051738a
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-11-30 13:17:24 +0530

    update feature added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,HTTPException
-from models.student_model import StudentModel
+from models.models import StudentModel,UpdateStudentModel
 from typing import List
 
 # Main App running the server 
@@ -8,9 +8,11 @@ app  = FastAPI()
 # Setting the container for storing data
 students : List[StudentModel] = []
 
+
 @app.get("/")
 def initial_route():
     return "Hello World!"
+
 
 @app.post("/add_student")
 async def add_user(student: StudentModel):
@@ -23,6 +25,25 @@ async def add_user(student: StudentModel):
     students.append(student)
     return {"message": "Student created successfully", "values": student}
 
+
 @app.get("/fetch_students")
 def fetchStudents():
     return {"students" : students}
+
+
+@app.put("/update_student")
+def updateStudent(student : UpdateStudentModel):
+    student_pointer = 0;
+    for existing_student in students:
+        if existing_student.id == student.id:
+
+            if(student.name != None):
+                existing_student.name = student.name
+
+            if(student.divison != None):
+                existing_student.divison = student.divison
+        
+        else:
+            raise HTTPException(status_code=404, detail="Student details not found.")
+    
+    return {"message" : "Student data updated successfully", "data" : student}
--- models/models.py
+++ models/models.py
@@ -5,3 +5,9 @@ class StudentModel(BaseModel):
     id : int
     name : str
     divison : Optional[int] = None 
+
+
+class UpdateStudentModel(BaseModel):
+    id : int 
+    name : Optional[str] = None 
+    divison : Optional[int] = None 

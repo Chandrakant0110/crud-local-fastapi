5260238c0786f85efb722e6776d2229e330cac16
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-11-30 13:03:17 +0530

    add students value check

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from models.student_model import StudentModel
 from typing import List
 
@@ -13,6 +13,16 @@ def initial_route():
     return "Hello World!"
 
 @app.post("/add_student")
-def add_user(student : StudentModel):
+async def add_user(student: StudentModel):
+    # Check if the student ID already exists
+    for existing_student in students:
+        if existing_student.id == student.id:
+            raise HTTPException(status_code=400, detail="Please enter a different userID.")
+    
+    # Append the student if no duplicates are found
     students.append(student)
-    return {"message" : "Student created successfully" , "values" : student}
+    return {"message": "Student created successfully", "values": student}
+
+@app.get("/fetch_students")
+def fetchStudents():
+    return {"students" : students}

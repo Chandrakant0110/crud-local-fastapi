bf87d158104d856df9f689eb6dc436dbdb1f0e88
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2024-11-30 13:42:40 +0530

    updates added

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI,HTTPException
 from models.models import StudentModel,UpdateStudentModel
 from typing import List
+import uvicorn
+import os
 
 # Main App running the server 
 app  = FastAPI()
+
+if __name__ == "__main__":
+    # Use the PORT environment variable if it exists, else default to 8000
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # Setting the container for storing data
 students : List[StudentModel] = []
